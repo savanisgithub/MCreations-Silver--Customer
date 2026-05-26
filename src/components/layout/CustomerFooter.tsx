804d4a2c8c83fce8e27d96f6779cb1af2c497fb1
savanisgithub <nilothma2000@gmail.com>
2026-05-26 14:36:26 +0530

    feat(ui): update footer to include logo and adjust padding

--- src/components/layout/CustomerFooter.tsx
+++ src/components/layout/CustomerFooter.tsx
@@ -1,14 +1,22 @@
 import { Box, Container, Divider, Typography } from '@mui/material';
-import DiamondIcon from '@mui/icons-material/Diamond';
+import Logo from '../../../public/logo.png';
 
 export default function CustomerFooter() {
     return (
         <Box sx={{ mt: 8, bgcolor: '#111827', color: 'white' }}>
-            <Container maxWidth="xl" sx={{ py: 5 }}>
+            <Container maxWidth="xl" sx={{ py: 2 }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, mb: 2 }}>
-                    <DiamondIcon color="secondary" />
-                    <Typography variant="h6" sx={{ fontWeight: 900 }}>
-                        Silver Aura
+                    <Box
+                        component="img"
+                        src={Logo}
+                        alt="MCreations Logo"
+                        sx={{
+                            height: 60,
+                            width: 'auto',
+                        }}
+                    />
+                    <Typography variant="h5" sx={{ fontWeight: 900 }}>
+                        MCreations
                     </Typography>
                 </Box>
 
